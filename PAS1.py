54d0d53eaf236970577a65a5da02ce29e2e1388e
Òscar Garcia Fernan <oscar.garcia.ferran.7e8@itb.cat>
2025-01-14 09:36:01 +0100

    Update PAS1.py

--- PAS1.py
+++ PAS1.py
@@ -6,15 +6,22 @@ correct_header = "precip\tMIROC5\tRCP60\tREGRESION\tdecimas\t1"
 # Ruta de la carpeta
 folder_path = "PRECIPITACIONS"
 
-# Recorrer todos los archivos en la carpeta
-for filename in os.listdir(folder_path):
-    if filename.endswith(".dat"):
-        file_path = os.path.join(folder_path, filename)
-        with open(file_path, 'r') as file:
-            # Leer la primera línea
-            first_line = file.readline().strip()
-            # Verificar si la cabecera es correcta
-            if first_line == correct_header:
-                print(f"{filename}: Cabecera correcta")
-            else:
-                print(f"{filename}: Cabecera incorrecta")
+# Ruta del archivo de log de errores
+error_log_path = "Error_log.log"
+
+# Abrir el archivo de log de errores en modo de escritura
+with open(error_log_path, 'w') as error_log:
+    # Recorrer todos los archivos en la carpeta
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".dat"):
+            file_path = os.path.join(folder_path, filename)
+            with open(file_path, 'r') as file:
+                # Leer la primera línea
+                first_line = file.readline().strip()
+                # Verificar si la cabecera es correcta
+                if first_line == correct_header:
+                    print(f"{filename}: Cabecera correcta")
+                else:
+                    error_message = f"{filename}: Cabecera incorrecta\n"
+                    print(error_message.strip())
+                    error_log.write(error_message)
